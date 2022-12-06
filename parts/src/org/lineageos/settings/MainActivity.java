e4f7ca2b4b3c6c4a8fbf05b150cef93506844002
xyyx <xyyx@mail.ru>
2022-12-06 11:59:19 +0000

    parts: Replace com.android.settingslib.collapsingtoolbar.R with com.android.settingslib.widget.R

--- parts/src/org/lineageos/settings/MainActivity.java
+++ parts/src/org/lineageos/settings/MainActivity.java
@@ -20,7 +20,7 @@ package org.lineageos.settings;
 
 import android.os.Bundle;
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class MainActivity extends CollapsingToolbarBaseActivity {
     @Override
